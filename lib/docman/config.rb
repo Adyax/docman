c135aa0b766c70e3c3daddef48f743611bffc5af
Alexander Tolstikov <atolstikov@adyax.com>
2017-04-12 22:07:52 +0300

    changed scenario to scenarios

--- lib/docman/config.rb
+++ lib/docman/config.rb
@@ -25,25 +25,33 @@ module Docman
       file = File.join(docroot_config_dir, config_file)
       if File.file?(file)
         config = YAML::load_file(file)
-        unless config.nil?
-          @config.deep_merge(config)
-          @config['version'] = config['version'].nil? ? 1 : config['version']
+        unless options[:config_repo].nil? || config.nil?
+          scenarios_path = File.join(docroot_config_dir, '/../', 'scenarios')
+          `rm -fR #{scenarios_path}` if File.directory? scenarios_path
+          GitUtil.clone_repo(options[:config_repo], scenarios_path, 'branch', options[:config_repo_branch], true, 1)
+          config = merge_scenarios_configs(config, scenarios_path)
         end
+      end
+      unless config.nil?
+        @config.deep_merge!(config)
+        @config['version'] = config['version'].nil? ? 1 : config['version']
+      end
 
-        unless options[:config_repo].nil?
-          scenariosPath = File.join(docroot_config_dir, '/../', 'scenarios')
-          unless config['scenario'].nil?
-            `rm -fR #{scenariosPath}` if File.directory? scenariosPath
-            GitUtil.clone_repo(options[:config_repo], scenariosPath, 'branch', options[:config_repo_branch], true, 1)
-            scenario_config_file = File.join(scenariosPath, 'scenarios', config['scenario'], 'config.yaml')
-            if File.file? scenario_config_file
-              scenario_config = YAML::load_file(scenario_config_file)
-              @config.deep_merge(scenario_config)
-            end
+      assign_to_self
+    end
+
+    def merge_scenarios_configs(config, scenarios_path)
+      scenarios_config = {}
+      if config['scenarios']
+        config['scenarios'].each do |scenario|
+          scenario_config_file = File.join(scenarios_path, 'scenarios', scenario, 'config.yaml')
+          if File.file? scenario_config_file
+            scenario_config = merge_scenarios_configs(YAML::load_file(scenario_config_file), scenarios_path)
+            scenarios_config.deep_merge!(scenario_config)
           end
         end
-        assign_to_self
       end
+      scenarios_config.deep_merge!(config)
     end
 
     def config_hash
